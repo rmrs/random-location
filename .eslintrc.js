cced5571c75195441979590c81786ea9509df7b8
Sagi Kedmi <git@sagi.io>
2019-09-28 09:52:54 +0300

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["jest"],
+  globals: {
+    process: true,
+    console: true,
+    module: true,
+    Promise: true,
+    exports: true,
+    Buffer: true,
+    Set: true
+  },
+  env: {
+    "jest/globals": true
+  }
+};
