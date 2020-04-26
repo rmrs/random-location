37148be4207a74366a4e9538f58afaf1038a58bd
Sagi Kedmi <git@sagi.io>
2020-04-26 16:48:54 +0300

    fix: use new prettier cmd --check

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const DEG_TO_RAD = Math.PI / 180.0;
 const THREE_PI = Math.PI * 3;
 const TWO_PI = Math.PI * 2;
 
-const toRadians = deg => deg * DEG_TO_RAD;
-const toDegrees = rad => rad / DEG_TO_RAD;
+const toRadians = (deg) => deg * DEG_TO_RAD;
+const toDegrees = (rad) => rad / DEG_TO_RAD;
 
 /*
 Given a centerPoint C and a radius R, returns a random point that is on the
